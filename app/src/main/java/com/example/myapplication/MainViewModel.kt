b60792a24638c19954978c51c7bfd8f9bfbbef74
Ibodwe <chlwjddn9181@naver.com>
2022-05-17 09:13:45 +0900

    tempbranch22

--- app/src/main/java/com/example/myapplication/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/MainViewModel.kt
@@ -9,6 +9,9 @@ class MainViewModel : ViewModel() {
 
     // 수정사항 이거 수정사항
     // this is TempBranch11
+    // this is Temp Branch 222
+    // 수정 완료
+
     private val _userData : MutableStateFlow<List<User>> = MutableStateFlow(emptyList())
     val userData = _userData
 
